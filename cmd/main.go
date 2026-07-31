71c1a1d4459ef5fbb3af1ee4d149571c42afa84f
miracle <miraclechukwu2011@gmail.com>
2026-07-31 20:29:32 +0100

    implemented gracefull shutdown in entry main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"multi-tenant-authorization-service/pkg/config"
 	"multi-tenant-authorization-service/pkg/db"
 	"multi-tenant-authorization-service/pkg/logger"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -21,11 +24,44 @@ func main() {
 	}
 	logger.Info("Connected to database")
 
-	// Start server
-	logger.Info("Starting server")
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", cfg.AppPort), mux); err != nil {
-		logger.Error("Failed to start server")
-		os.Exit(1)
+	//
+	server := &http.Server{
+		Addr:         ":" + cfg.AppPort,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
 	}
-}
 
+	// Start server safely
+	go func() {
+		logger.Info("Egento API starting on port " + cfg.AppPort)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Error("Failed to start server")
+			os.Exit(1)
+		}
+	}()
+
+	// Shutdown signal listener
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	// Graceful shutdown context
+	logger.Info("Shutting down server")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Shutdown server
+	if err := server.Shutdown(ctx); err != nil {
+		logger.Error("Graceful shutdown failed")
+		server.Close()
+	}
+
+	// Close database connection
+	if err := db.DB.Close(); err != nil {
+		logger.Error("Database connection close failed")
+	}
+
+	logger.Info("Server exiting gracefully")
+}
